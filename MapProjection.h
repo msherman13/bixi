0fcb915fde614f549d400266f05395f6b2918fe5
Miles Sherman <miles@miles-sherman.com>
2017-06-04 11:31:34 -0400

    lambert conformal projection seems functional

--- MapProjection.h
+++ MapProjection.h
@@ -3,6 +3,8 @@
 
 namespace MapProjection
 {
+    constexpr float epsilon = 0.0001;
+
     struct GeographicCoord
     {
         float latitude  = 0.0;
@@ -39,11 +41,8 @@ namespace MapProjection
 
     // mercator projection
     // see: http://mathworld.wolfram.com/MercatorProjection.html
-    CPixelArray::Coordinate Coord3dTo2d(Coord3d coord_3d)
+    CPixelArray::Coordinate MercatorProjection(GeographicCoord geog)
     {
-        // first convert to latitude and longitude
-        GeographicCoord geog = Coord3dToGeographic(coord_3d);
-
         const float lambda_o = 0.0;
 
         CPixelArray::Coordinate ret;
@@ -51,5 +50,52 @@ namespace MapProjection
         ret.y = 0.5 * log((1 + sin(geog.latitude)) / (1 - sin(geog.latitude)));
 
         return ret;
+    }
+
+    CPixelArray::Coordinate MercatorProjection3d(Coord3d coord_3d)
+    {
+        GeographicCoord geog = Coord3dToGeographic(coord_3d);
+
+        return MercatorProjection(geog);
+    }
+
+    // lambert conformal conic projection
+    // see: http://mathworld.wolfram.com/LambertConformalConicProjection.html
+    CPixelArray::Coordinate LambertProjection(GeographicCoord geog)
+    {
+        CPixelArray::Coordinate ret;
+
+        const float std_par_1 = M_PI / 6; // 30 degrees
+        const float std_par_2 = M_PI / 3; // 60 degrees
+        const float ref_lat   = M_PI / 2; // 90 degrees (north pole)
+        const float ref_long  = 0;
+
+        float n = log( cos(std_par_1) * (1 / cos(std_par_2) ) ) /
+                  log( tan(M_PI / 4 + std_par_2 / 2) * ( 1 / tan(M_PI / 4 + std_par_1 / 2) ) );
+
+        float f = ( cos(std_par_1) * pow( tan(M_PI / 4 + std_par_1 / 2), n) ) / n;
+
+        // cot of 0 is 0, but the tangent part returns inf which evaluates to nan
+        float rho_tan = abs(geog.latitude - M_PI / 2) < epsilon || abs(-geog.latitude - M_PI / 2) < epsilon ?
+                        0 :
+                        1 / tan(M_PI / 4 + geog.latitude / 2);
+        float rho = f * pow( rho_tan, n );
+
+        float rho_0_tan = abs(ref_lat - M_PI / 2) < epsilon || abs(-ref_lat - M_PI / 2) < epsilon ?
+                          0 :
+                          1 / tan(M_PI / 4 + ref_lat / 2);
+        float rho_0 = f * pow( rho_0_tan, n );
+
+        ret.x = rho * sin( n * (geog.longitude - ref_long) );
+        ret.y = rho_0 - rho * cos( n * (geog.longitude - ref_long) );
+
+        return ret;
+    }
+
+    CPixelArray::Coordinate LambertProjection3d(Coord3d coord_3d)
+    {
+        GeographicCoord geog = Coord3dToGeographic(coord_3d);
+
+        return LambertProjection(geog);
     }
 };
